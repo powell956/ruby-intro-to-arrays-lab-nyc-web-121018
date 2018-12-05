e83862b29766f24ef9bb1d4b3a705fb925e37902
powell956 <powell956@gmail.com>
2018-12-05 07:08:13 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  arr = []
+end
+
+def array_with_two_elements
+  arr = [{}, {}]
+end
+
+def first_element(arr)
+  first = arr[0]
+end
+
+def third_element(arr)
+  third = arr[2]
+end
+
+def last_element(arr)
+  last = arr[-1]
+end
+
+def length_of_array(arr)
+  length = arr.length
+end
+
+def first_element_with_array_methods(arr)
+  first = arr.first
+end
+
+def last_element_with_array_methods(arr)
+  last = arr.last
+end
